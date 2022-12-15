a4bba4a9810dd6395f979f2ac9440d23ffb1826b
ThanhTrieuVo <ttrieu@lunageo.com>
2022-12-15 12:34:43 -0500

    highlight selected menu item

--- src/components/layout/sidebar/SidebarMenu.js
+++ src/components/layout/sidebar/SidebarMenu.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { SlideMenu } from "primereact/slidemenu";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useMatch } from "react-router-dom";
 const SidebarMenu = () => {
   const isShowSlidebar = useSelector((state) => state.ui.slidebarIsVisible);
   const navigate = useNavigate();
@@ -8,6 +8,7 @@ const SidebarMenu = () => {
     {
       label: "Trang Chủ",
       icon: "pi pi-fw pi-home",
+      className: useMatch('/app/home') ? 'surface-hover' : '',
       command: () => {
         navigate("/app/home");
       },
@@ -15,6 +16,7 @@ const SidebarMenu = () => {
     {
       label: "Khách Hàng",
       icon: "pi pi-fw pi-user",
+      className: useMatch('/app/customers') ? 'surface-hover' : '',
       command: () => {
         navigate("/app/customers");
       },
@@ -22,6 +24,7 @@ const SidebarMenu = () => {
     {
       label: "Phụ tùng",
       icon: "pi pi-fw pi-box",
+      className: useMatch('/app/spare-part') ? 'surface-hover' : '',
       command: () => {
         navigate("/app/spare-part");
       },
@@ -29,6 +32,7 @@ const SidebarMenu = () => {
     {
       label: "Chu kỳ bảo dưỡng",
       icon: "pi pi-fw pi-car",
+      className: useMatch('/app/maintainance-cycles') ? 'surface-hover' : '',
       command: () => {
         navigate("/app/maintainance-cycles");
       },
@@ -36,6 +40,7 @@ const SidebarMenu = () => {
     {
       label: "Phiếu bão dưỡng / sửa chữa",
       icon: "pi pi-fw pi-cog",
+      className: useMatch('/app/repair') ? 'surface-hover' : '',
       command: () => {
         navigate("/app/repair");
       },
